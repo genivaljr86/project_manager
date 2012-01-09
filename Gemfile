54e4f37c0688386e7e92c926033b829ebba428a9
Ernane <ernane.sena@gmail.com>
2012-01-09 08:57:03 -0300

    Inicio

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.3'
+gem 'sqlite3'
+gem 'devise'
+
+
+
+group :assets do
+  gem 'sass-rails',   '~> 3.1.5'
+  gem 'coffee-rails', '~> 3.1.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+
+group :test do
+  # Pretty printed test output
+  gem 'turn', '0.8.2', :require => false
+end
+
+group :development, :test do
+  gem 'nifty-generators'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjectManager::Application.config.secret_token = '2784a496e174e36772dfc8950c0e21075c46e4804a735c7f18be2c32012f63417e9e21b86057429c2da86e056e75415ec8a3dc0228d9fd9ae1e42ae833053d44'
