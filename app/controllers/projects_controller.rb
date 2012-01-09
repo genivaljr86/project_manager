35381691bd13c055ba2169d98083a86a17a089c8
Ernane <ernane.sena@gmail.com>
2012-01-09 11:29:21 -0300

    Add find_project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,10 +1,12 @@
 class ProjectsController < ApplicationController
+  before_filter :find_project, :only => [:show, :edit, :update, :destroy]
+
   def index
     @projects = Project.all
   end
 
   def show
-    @project = Project.find(params[:id])
+
   end
 
   def new
@@ -12,7 +14,7 @@ class ProjectsController < ApplicationController
   end
 
   def edit
-    @project = Project.find(params[:id])
+
   end
 
   def create
@@ -26,7 +28,6 @@ class ProjectsController < ApplicationController
   end
 
   def update
-    @project = Project.find(params[:id])
     if @project.update_attributes(params[:project])
       flash[:notice] = t("update_sucess")
       redirect_to @project
@@ -41,4 +42,14 @@ class ProjectsController < ApplicationController
     flash[:notice] = t("destroy_sucess")
     redirect_to projects_path
   end
+
+
+  private
+  def find_project
+    @project = Project.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    flash[:alert] = t("project_not_found")
+    redirect_to projects_path
+  end
+
 end
