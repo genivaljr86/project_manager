4314ff3ee3b2324b72b03a5833cf74f19f5ff229
Ernane <ernane.sena@gmail.com>
2012-02-07 16:38:40 -0300

    Confi Devise Production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,4 +57,6 @@ ProjectManager::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  config.action_mailer.default_url_options = { :host => 'talentomanager.com.br' }
 end
